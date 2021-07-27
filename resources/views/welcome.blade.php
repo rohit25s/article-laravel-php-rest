9c1232f322186f3de04ad4525b5798f54b0a1afc
Rohit Sah <rohit.sah@meesho.com>
2021-07-27 13:37:22 +0530

    home top bar changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,9 +27,9 @@
                 <div class="hidden fixed top-0 right-0 px-10 py-4 sm:block">
                     @auth
                         <a href="{{ url('/create_article') }}" class="text-lg-right dark:text-gray-400 underline">Create Article</a>
-                        <a href="{{ url('/article') }}" class="text-lg-right dark:text-gray-400 underline">Articles</a>
-                        <a href="{{ url('/author') }}" class="text-lg-right dark:text-gray-400 underline">Authors</a>
-                        <a href="{{ url('/home') }}" class="text-lg-right dark:text-gray-400 underline">Home</a>
+                        <a href="{{ url('/article') }}" class="ml-2 text-lg-right dark:text-gray-400 underline">Articles</a>
+                        <a href="{{ url('/author') }}" class="ml-2 text-lg-right dark:text-gray-400 underline">Authors</a>
+                        <a href="{{ url('/home') }}" class="ml-2 text-lg-right dark:text-gray-400 underline">Home</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm dark:text-gray-400 underline">Log in</a>
 
